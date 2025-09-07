3ade3bcac629d21a6c1bcc1f97b50c492b3f21b7
hernanveyret <hernanveyret@hotmail.com>
2025-09-07 20:35:46 -0300

    prueba 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { useState } from 'react'
 import { useZxing } from 'react-zxing';
+import { useForm } from "react-hook-form";
 
 import './App.css'
 
 function App() {
-const [result, setResult] = useState(null);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+   } = useForm();
+
+  const [result, setResult] = useState(null);
+  const [ productos, setProductos ] = useState([]);
   const { ref } = useZxing({
     onDecodeResult(result) {
       setResult(result.getText());
@@ -14,8 +22,16 @@ const [result, setResult] = useState(null);
   return (
     <div>
       <h1>Escáner de Códigos</h1>
-      <video ref={ref} width="100%" />
+      <video ref={ref} width="50%" />
       {result && <p>Resultado: {result}</p>}
+      <form>
+        <input type="text" name="numerobarra" defaultValue={result && result}/>
+        <input type="text" name="nombreProducto" placeholder='Nombre' />
+        <input type="text" name="descripcion" placeholder='Descripcion' />
+        <input type='text' name="precioUnit" placeholder='Precio Unit.' />
+        <input type='text' name="precioXdos" placeholder='Precio X 2.' />
+        <button type='submit'>CARGAR</button>
+      </form>
     </div>
   );
 }
